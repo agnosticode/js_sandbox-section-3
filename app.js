7a9b0690fb10f587bb5dee996f5bc1a6812556ce
Satinder Singh <agnosticode@gmail.com>
2018-03-18 16:42:43 +0530

    Event listeners and the event object

--- app.js
+++ app.js
@@ -1,56 +1,36 @@
-//Replace Element
+// document.querySelector('.clear-tasks').addEventListener('click',function(e){
+//     console.log('Hello World');
+//     e.preventDefault();
+// });
+
+//Put named function
+document.querySelector('.clear-tasks').addEventListener('click',onClick);
+
+function onClick(e){
+    //console.log('Hello world');
+    let val;
+
+    val=e;
+
+    //Event target element
+    val=e.target;
+    val=e.target.id;
+    val=e.target.className;
+    val=e.target.classList;
+
+    e.target.innerText="HELLO";
+
+    //Event type
+    val=e.type;
+    //TypeStamp
+    val=e.timeStamp;
+    //Event coordinate relative to window
+    val=e.clientY;
+    val=e.clientX;
+    //Event coordinate relative to element itself
+    val=e.offsetY;
+    val=e.offsetX;
 
 
-//Create Element
-const newHeading=document.createElement('h2');
-//Add id
-newHeading.id='task-title';
-//New text node
-newHeading.appendChild(document.createTextNode('Task List'));
-
-//Get the old heading
-const oldHeading=document.getElementById('task-title');
-//Parent
-const cardAction=document.querySelector('.card-action');
-
-//Replace
-cardAction.replaceChild(newHeading,oldHeading);
-
-//Remove element
-const lis=document.querySelectorAll('li');
-const list=document.querySelector('ul');
-
-//Remove list item
-lis[0].remove();
-
-//Remove child element
-list.removeChild(lis[3]);
-
-//Classes and Attribute
-const firstLi=document.querySelector('li:first-child');
-const link=firstLi.children[0];
-
-let val;
-
-//CLASSES
-val=link.className;//String of classes
-val=link.classList;//DOM token list
-val=link.classList[0];
-//Add class
-link.classList.add('test');
-//remove class
-link.classList.remove('test');
-val=link;
-
-//ATTRIBUTES
-//get attribute
-val=link.getAttribute('href');
-//setAttribute
-val=link.setAttribute('href','https://www.google.com');
-//check if attribute
-val=link.hasAttribute('href');//true
-//remove attribute
-//val=link.removeAttribute('href');
-val=link;
-
-console.log(val);
+    console.log(val);
+};
